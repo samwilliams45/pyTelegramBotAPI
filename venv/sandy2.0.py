8d754172c41aac77ca3f039d17c75a42bc8c9a1b
samwilliams45 <sam_williams45@yahoo.com>
2023-01-19 15:13:47 -0600

    commit

--- venv/sandy2.0.py
+++ venv/sandy2.0.py
@@ -1,6 +1,5 @@
 import os
 import telebot
-import
 
 API_KEY = os.getenv('5608601651:AAHY_d3XgJ14UeEvbT44CWYnexE1UWpIhRQ')
 bot = telebot.TeleBot(API_KEY)
@@ -15,9 +14,9 @@ def start(message):
 def help(message):
     bot.send_message(message.chat.id, "what is Guicci")
 
-@bot.message_handler(func=Staging_list)
-def stage(message):
-request = message.text.split()[1]
-data = 
+#@bot.message_handler(func=Staging_list)
+#def stage(message):
+#request = message.text.split()[1]
+#data = 
 bot.polling()
 
